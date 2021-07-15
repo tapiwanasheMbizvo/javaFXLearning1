f7b51cb7a31ae3c7c4ab7d478fbae9310beb1567
tapiwanasheMbizvo <tapiwambizvo@gmail.com>
2021-07-15 06:38:50 +0200

    initial commit

---
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication1;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author tmbizvo
+ */
+public class JavaFXApplication1 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage)  {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            
+            
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+            // StackPane root = new StackPane();
+        } catch (IOException ex) {
+            Logger.getLogger(JavaFXApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+      
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
